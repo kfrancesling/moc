385bc00f52448f7d02fd90b9694f0c148f1e7bb5
Oscar Lingard <lingardo@Oscars-MacBook-Pro.local>
2020-12-23 17:23:19 +0000

    Creation of js. Implementing flex css. Updating some card titles

---
+++ cardHandler.js
@@ -0,0 +1,53 @@
+card_image_element = document.getElementById("flip-card-front-image")
+information_text_element = document.getElementById("information-text")
+information_image_element = document.getElementById("information-image")
+
+function card(value, suit) {
+    this.value = value,
+    this.suit = suit
+};
+
+function deck() {
+    deckOfCards = []
+    suits = ['coins','hearts','tools','vessels']
+    values = ['two','three','four','five','six','seven','eight','nine','ten','jack','queen','king','ace']
+    for (suit of suits) {
+        for (value of values) {
+            deckOfCards.push(
+                new card(value, suit)
+            )
+        }
+    }
+    deckOfCards.push(new card("joker", "coins"))
+    deckOfCards.push(new card("joker", "hearts"))
+
+    return deckOfCards
+}
+
+cardDeck = new deck();
+
+function shuffle(deck) {
+    randomCard = deck[Math.floor(Math.random() * deck.length)];
+    return randomCard
+}
+
+function clickHandler() {
+    randomCard = shuffle(cardDeck)
+    cardValue = randomCard.value
+    cardSuit = randomCard.suit
+    console.log(cardSuit,cardValue)
+    
+    card_image_element.src = `cardimages/${cardValue}_${cardSuit}.jpg`
+
+    information_image_element.src = `cardimages/${cardValue}_${cardSuit}.jpg`
+    information_text_element.innerHTML = descriptions.suits.find(
+        obj => {
+            return obj.suit = cardSuit
+        }
+    ).description
+
+    
+
+}
+
+document.getElementById("shuffle-button").addEventListener("click",clickHandler)
---
+++ descriptions.js
@@ -0,0 +1,34 @@
+descriptions = {
+    values: [
+        {
+            value: "ace",
+            description: "this is a test"
+        },
+        {
+            value: "2",
+            description: "this is a test"
+        },
+        {
+            value: "3",
+            description: "this is a test"
+        }
+    ],
+    suits: [
+        {
+            suit: "coins",
+            description: "fjewqiopioqf"
+        },
+        {
+            suit: "hearts",
+            description: "fjewqiopioqf"
+        },
+        {
+            suit: "tools",
+            description: "fjewqiopioqf"
+        },
+        {
+            suit: "vessels",
+            description: "fjewqiopioqf"
+        }
+    ]
+}
